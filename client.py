ea485728f3860341ff88378b144ff6a9889720d2
Kabariya Krish Darshakbhai <krishkabariya007@gmail.com>
2025-06-18 14:37:05 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 # pip install openai 
 # if you saved the key under a different environment variable name, you can do something like:
 client = OpenAI(
-  api_key="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 ",
+  api_key="my api key ",
 )
 
 completion = client.chat.completions.create(
